6a6fe6104bd88f953ec2503eb6bc7fb38f121b55
Zino Frankie <132056792+zinofrankie77@users.noreply.github.com>
2025-06-19 14:36:02 +0100

    Update Headersocials.jsx

--- src/Components/Header/Headersocials.jsx
+++ src/Components/Header/Headersocials.jsx
@@ -5,7 +5,7 @@ import {BsGithub} from 'react-icons/bs'
 const Headersocials = () => {
   return (
     <div className='header__socials'>
-      <a href="https://www.linkedin.com/in/frank-ugbobor-a908431ab/" target="_blank"><BsLinkedin/></a>
+      <a href="https://www.linkedin.com/in/frank-zino-a908431ab/" target="_blank"><BsLinkedin/></a>
       <a href="https://github.com/zinofrankie77" target='_blank'><BsGithub/></a>
     </div>
   )
